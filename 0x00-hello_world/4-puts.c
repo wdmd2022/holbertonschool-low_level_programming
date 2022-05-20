6b7fb3e6003b81817a08ce6bc6d13124e29f81fb
wdmd2022 <doug@dougrules.com>
2022-05-20 11:21:28 -0700

    upload script 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Initializing string */
+char str1[] = "\\"Programming is like building a multilingual puzzle";
+
+	/* writing string to standard output */
+puts(str1);
+
+	return (0)
+}
