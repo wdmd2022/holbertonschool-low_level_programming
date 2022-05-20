6cdb047d5873456ffc71a6ed594cf30b21921b69
wdmd2022 <doug@dougrules.com>
2022-05-20 12:46:58 -0700

    upload script 6 one more time just to be fun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,11 @@
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\n"
+		"Size of a long int: %d byte(s)\n"
+		"Size of a long long int: %d byte(s)\n"
+		"Size of a float: %d byte(s)\n",
+		sizeof(char), sizeof(int), sizeof(long int),
+		sizeof(long long int), sizeof(float));
 	return (0);
 }
