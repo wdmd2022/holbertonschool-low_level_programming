ecf646cdcc63e2e87e95380de18cb0fb5fafb686
wdmd2022 <doug@dougrules.com>
2022-05-20 12:39:11 -0700

    upload 6 again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-printf("Size of a char: %l byte(s)\nSize of an int: %l byte(s)\nSize of a long int: %l byte(s)\nSize of a long long int: %l byte(s)\nSize of a float: %l byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
 	return (0);
 }
