fb8bb0cc0f65e6b9e38b8ac471c2d89bf16e031c
wdmd2022 <doug@dougrules.com>
2022-05-20 12:30:45 -0700

    upload script 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %s byte(s)\nSize of an int: %s byte(s)\nSize of a long int: %s byte(s)\nSize of a long long int: %s byte(s)\nSize of a float: %s byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float);
+	return (0);
+}
