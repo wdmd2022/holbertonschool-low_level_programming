45cbebf5583470ee93121e16ef6d5a298645e8a5
wdmd2022 <doug@dougrules.com>
2022-05-23 11:46:12 -0700

    upload script 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+		int n;
+
+	for (n = 65; n <= 123; n++)
+		if (n <= 90)
+			putchar(n);
+		else if ((n >= 97) && (n <= 122))
+			putchar(n);
+		else if (n == 123)
+			putchar(10);
+		return (0);
+}
