c49505b958a5b801ca3e43120d0a11967d3896ed
wdmd2022 <doug@dougrules.com>
2022-05-23 11:48:05 -0700

    fix indentation script 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 	{
-		int n;
+	int n;
 
 	for (n = 65; n <= 123; n++)
-		if (n <= 90)
-			putchar(n);
-		else if ((n >= 97) && (n <= 122))
-			putchar(n);
-		else if (n == 123)
-			putchar(10);
-		return (0);
+	if (n <= 90)
+		putchar(n);
+	else if ((n >= 97) && (n <= 122))
+		putchar(n);
+	else if (n == 123)
+		putchar(10);
+	return (0);
 }
