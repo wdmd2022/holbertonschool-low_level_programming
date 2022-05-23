1aa69f96afb87ea648cd532dfb925dc4999da87c
wdmd2022 <doug@dougrules.com>
2022-05-23 12:11:36 -0700

    upload scripts 5 and 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	putchar(10);
+	return (0);
+}
