94aeecfbe5fcc9b50ba83c53cf45729a5156d910
wdmd2022 <doug@dougrules.com>
2022-05-23 13:11:22 -0700

    upload script 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	for (n = 97; n <= 102; n++)
+		putchar(n);
+	putchar(10);
+	return (0);
+}
