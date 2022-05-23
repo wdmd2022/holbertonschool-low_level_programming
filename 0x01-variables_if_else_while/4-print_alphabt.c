1296ed404e14c996cf68db629ada93593328749e
wdmd2022 <doug@dougrules.com>
2022-05-23 12:05:32 -0700

    upload script 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+	int n;
+
+	for (n = 97; n <= 123; n++)
+		if ((n != 101) && (n != 113))
+			putchar(n);
+	putchar(10);
+	return (0);
+}
