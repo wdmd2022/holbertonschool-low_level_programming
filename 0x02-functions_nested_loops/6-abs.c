5f10829ea31a000cc67843c67eb650981d9131a4
wdmd2022 <doug@dougrules.com>
2022-05-24 12:19:03 -0700

    upload script 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - tell is a characer is a lower/upper case letter  or not
+ *
+ * @n: value of int provided
+ *
+ * Return: absolute value of int provided
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		n = (n * -1);
+		return (n);
+	}
+}
