f410104feda960684404e63fba2382a721f894db
wdmd2022 <doug@dougrules.com>
2022-05-24 16:12:03 -0700

    upload script 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @n: - integer supplied in argument
+ * @m: - integer supplied in argument
+ * Return: the result of addition
+ */
+
+int add(int n, int m)
+{
+	int sum = n + m;
+
+	return (sum);
+}
