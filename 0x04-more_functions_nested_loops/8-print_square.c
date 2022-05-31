274a65d1622e2f4027a425affeb3a6f5953b8ba1
wdmd2022 <doug@dougrules.com>
2022-05-31 15:38:59 -0700

    upload script 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line
+ *
+ * @size: integer, size of square
+ *
+ * Return: Always 0
+ */
+void print_square(int size)
+{
+	int width;
+	int height;
+
+	if (size > 0)
+	{
+		for (height = 1; height <= size; height++)
+		{
+			for (width = 0; width <= size; width++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+	}
+	else
+		_putchar(10);
+}
