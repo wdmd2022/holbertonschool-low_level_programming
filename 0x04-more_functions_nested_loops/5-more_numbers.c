d07844f403127cf2849e7df911cfbe1a04ff08db
wdmd2022 <doug@dougrules.com>
2022-05-31 11:55:31 -0700

    upload script 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers 0 to 14, ten times
+ *
+ * Return: always 0
+ */
+void more_numbers(void)
+{
+	int tentimer;
+	int fourteentimer;
+
+	for (tentimer = 0; tentimer < 10; tentimer++)
+	{
+		for (fourteentimer = 0; fourteentimer <= 14; fourteentimer++)
+		{
+			if (fourteentimer > 10)
+				_putchar((fourteentimer / 10) + 48);
+			_putchar((fourteentimer % 10) + 48);
+		}
+		_putchar(10);
+	}
+}
