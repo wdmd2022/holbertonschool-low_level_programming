b6dbffb74a6f9646db1a1051d25034f43d29016d
wdmd2022 <doug@dougrules.com>
2022-06-01 09:39:03 -0700

    upload function 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string pointed to by *s
+ *
+ * @s: string being pointed to
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int lengthvariable;
+
+	for (lengthvariable = 0; *s != '\0'; s++)
+	{
+		lengthvariable++;
+	}
+	return (lengthvariable);
+}
