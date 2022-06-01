963dce31a3c17bd6944e535b2fa701beeb8d465d
wdmd2022 <doug@dougrules.com>
2022-06-01 11:46:35 -0700

    upload function 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ *
+ * @str: the string provided
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar(10);
+}
