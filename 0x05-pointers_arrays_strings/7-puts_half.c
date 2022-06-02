67398a2fbd031994f68c1d5e851c9f14df06c8f9
wdmd2022 <doug@dougrules.com>
2022-06-02 12:49:57 -0700

    upload function 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ *
+ * @str: the string provided
+ *
+ * Return: Always 0
+ */
+void puts_half(char *str)
+{
+	int i;
+	int length = 0;
+	int secondhalf;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	length--;
+
+	if (length % 2 == 0)
+	{
+		secondhalf = length / 2;
+	}
+	else
+		secondhalf = ((length / 2) + 1);
+
+	for (i = secondhalf; i <= length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(10);
+}
