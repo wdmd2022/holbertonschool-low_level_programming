51ecb960785258a2659bf59e0d165419d1366257
wdmd2022 <doug@dougrules.com>
2022-06-02 13:52:08 -0700

    upload function 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string pointed to by src, including the terminating
+ * null byte, to the buffer pointed to by dest
+ *
+ * @dest: buffer pointed to
+ * @src: string pointed to
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int celltocopy = 0;
+
+	while (src[celltocopy])
+	{
+		dest[celltocopy] = src[celltocopy];
+		celltocopy++;
+	}
+	dest[celltocopy] = '\0';
+	return (dest);
+}
