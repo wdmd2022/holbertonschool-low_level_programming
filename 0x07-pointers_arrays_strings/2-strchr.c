1ef69d4feb335364a9987dfb270ee9aeace9d552
wdmd2022 <doug@dougrules.com>
2022-06-07 11:58:05 -0700

    upload function 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - finds a character in a string
+ *
+ * @s: the string we look at
+ * @c: the character we are earnestly searching for
+ *
+ * Return: a pointer to the first occurence of c, or the null byte
+ * if c does not occur in s
+ */
+char *_strchr(char *s, char c)
+{
+	while (s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	return ('\0');
+}
