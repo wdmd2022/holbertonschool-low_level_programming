8b0828489b1237ed78d5565686aea54a3556ee6e
wdmd2022 <doug@dougrules.com>
2022-06-07 15:31:58 -0700

    upload function 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search a string for any of a set of bytes
+ *
+ * @s: a string which we look at
+ * @accept: the set of bytes (in string form)
+ *
+ * Return: a pointer to the byte in s that matches one of the bytes
+ * in accept, or else NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	while (*s++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+		}
+	}
+	return (0);
+}
