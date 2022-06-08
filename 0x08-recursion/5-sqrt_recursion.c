1195c19ef987f044e87759fc9e16dd186f092daa
wdmd2022 <doug@dougrules.com>
2022-06-08 14:31:29 -0700

    upload 5 effortlessly

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,33 @@
 #include "main.h"
 
 /**
- * function_name - description
- * @variable: definition
- *
- * Return: Always 0
+ * who_let_the_roots_out - checks for a square root
+ * @rootytooty: the starting point integer
+ * @freshfruity: the number we are seeking the square root for
+ * Return: int, the natural square root, or -1 if doesn't exist)
  */
-prototypehere
+int who_let_the_roots_out(int rootytooty, int freshfruity)
 {
-	
+	if (rootytooty * rootytooty == freshfruity)
+	{
+		return (rootytooty);
+	}
+	if (rootytooty * rootytooty > freshfruity)
+	{
+		return (-1);
+	}
+	return (who_let_the_roots_out(rootytooty + 1, freshfruity));
+}
+/**
+ * _sqrt_recursion - looks for a square root
+ * @n: the number
+ * Return: integer, the natural square root of (if it doesn't exist, return -1)
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (who_let_the_roots_out(0, n));
 }
