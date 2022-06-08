6bd974e49a887dcab9427f24c7151a6b5c9f3047
wdmd2022 <doug@dougrules.com>
2022-06-08 11:26:16 -0700

    upload 1 for test

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
- * function_name - description
- * @variable: definition
+ * _print_rev_recursion - prints a string in reverse
+ * @s: the string whose life we're going to flip-turn upside-down
  *
  * Return: Always 0
  */
-prototypehere
+void _print_rev_recursion(char *s)
 {
-	
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(++s);
+	_putchar(*s);
 }
