72c9d5fbb4ecff6d6534513ecf4d80ee1b37ba7c
wdmd2022 <doug@dougrules.com>
2022-06-08 14:54:33 -0700

    upload 6 which was a breeze

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,35 @@
 #include "main.h"
 
 /**
- * function_name - description
- * @variable: definition
+ * number_chumber - checks to see if an int is prime
+ * @nummie: int we are decrementing to check
+ * @tummie: number to be checked
  *
- * Return: Always 0
+ * Return: 1 if prime, otherwise 0
  */
-prototypehere
+number_chumber(int nummie, int tummie)
 {
-	
+	if (nummie == 1)
+	{
+		return (1);
+	}
+	if (tummie % nummie == 0)
+	{
+		return (0);
+	}
+	return (number_chumber(nummie - 1, tummie));
+}
+
+/**
+ * is_prime_number - returns 1 if the input int is a prime number
+ * @n: an integer to check
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (number_chumber(n / 2, n));
 }
