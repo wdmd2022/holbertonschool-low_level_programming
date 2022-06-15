965fa9fad1f743377effbdf75e7cdf1a8cfd48f5
wdmd2022 <doug@dougrules.com>
2022-06-15 12:28:52 -0700

    upload 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,22 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * function_name - what the function does
+ * main - starts the function that multiplies two integers
  *
- * @variable:
- * @variable:
+ * @argc: number of arguments
+ * @argv: pointer to array of strings
  *
- * Return: what it returns
+ * Return: 0 on success, 1 on error
  */
-
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%i\n", atoi(argv[1])*atoi(argv[2]));
+	return (0);
+}
