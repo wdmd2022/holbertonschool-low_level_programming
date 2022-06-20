87ca0d7e2c998fb6537d27f902581a1e53e565e8
wdmd2022 <doug@dougrules.com>
2022-06-20 12:01:26 -0700

    remove extra lines

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,34 +12,18 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *new;
-	int s1count = 0;
-	int s2count = 0;
-	int newlength;
-	int x;
-	int y;
+	int x, y, newlength, s1count = 0, s2count = 0;
 
 	if (s1 == NULL)
-	{
 		s1count = 0;
-	}
 	else
-	{
 		while (s1[s1count])
-		{
 		s1count++;
-		}
-	}
 	if (s2 == NULL)
-	{
 		s2count = 0;
-	}
 	else
-	{
 		while (s2[s2count])
-		{
 		s2count++;
-		}
-	}
 	newlength = s1count + s2count + 1;
 	new = (char *)malloc(newlength * sizeof(char));
 	if (new == NULL)
@@ -48,14 +32,10 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 	for (x = 0; x < s1count; x++)
-	{
 		new[x] = s1[x];
-	}
 	for (y = 0; y < s2count; y++)
-	{
 		new[x] = s2[y];
 		x++;
-	}
 	new[x] = '\0';
 	return (new);
 }
