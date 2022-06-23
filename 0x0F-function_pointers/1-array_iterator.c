55958466f2889a46d131c2d0ef79af7ddb69eb32
wdmd2022 <doug@dougrules.com>
2022-06-23 13:20:42 -0700

    upload 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - executes a function (param) on each element of an array
+ *
+ * @array: the array
+ * @size: size of the array
+ * @action: pointer to the function
+ *
+ * Return: nothing at all
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int county;
+
+	if ((array == NULL) || (action == NULL))
+	{
+		return;
+	}
+	for (county = 0; county < size; county++)
+	{
+		action(array[county]);
+	}
+}
