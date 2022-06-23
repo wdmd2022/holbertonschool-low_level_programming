df288c9e8e5d0bebf0812fa06f1ea242227683a8
wdmd2022 <doug@dougrules.com>
2022-06-22 19:06:12 -0700

    upload 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: a pointer to a dog structure
+ * Return: nothing because it is void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+	free(d);
+}
