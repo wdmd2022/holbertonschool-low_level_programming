8cb4a9e033975e00c32ca371cf68df8175964096
wdmd2022 <doug@dougrules.com>
2022-07-12 16:20:40 -0700

    upload 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 
 /**
  * listint_len - returns the number of elements in a listin_t int
- * @h - pointer to the first element
+ * @h: pointer to the first element
  * Return: the number of elements
  */
 size_t listint_len(const listint_t *h)
