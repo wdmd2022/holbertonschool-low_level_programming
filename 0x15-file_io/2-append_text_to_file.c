c665b085b18004383a1a1e8c6e627acaea8a387b
wdmd2022 <doug@dougrules.com>
2022-07-19 10:01:40 -0700

    fix value check error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == !NULL)
+	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i]; i++)
 		{
