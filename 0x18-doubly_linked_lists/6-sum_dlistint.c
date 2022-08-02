68f46fc084e24ddb4084973c06f7ca8dd259dbfc
wdmd2022 <doug@dougrules.com>
2022-08-02 06:29:45 -0700

    add last node sum

--- 0x18-doubly_linked_lists/6-sum_dlistint.c
+++ 0x18-doubly_linked_lists/6-sum_dlistint.c
@@ -10,7 +10,7 @@ int sum_dlistint(dlistint_t *head)
 	int sum = 0;
 	if (head == NULL)
 		return (sum);
-	while (head->next != NULL)
+	while (head != NULL)
 	{
 		sum += head->n;
 		head = head->next;
