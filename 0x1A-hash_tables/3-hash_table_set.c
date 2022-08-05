9fb03efb57664e758bd73b5a5c72013f190b3f9f
wdmd2022 <doug@dougrules.com>
2022-08-04 19:59:00 -0700

    upload 3 style compliant

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	
+
 	tempeh = ht->array[index];
 	if (tempeh && strcmp(key, tempeh->key) == 0)
 	{
